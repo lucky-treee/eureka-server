286b67106eaae14e88a1dd693fa3eea04361ecf7
gowjr207@gmail.com <gowjr207@gmail.com>
2022-12-15 00:43:33 +0900

    [init] eureka server

---
+++ eureka-server/src/test/java/com/luckytree/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckytree.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
